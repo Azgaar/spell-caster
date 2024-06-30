a4484b6868cfa2b34c9bae983d51e675d2dab25c
Azgaar <azgaar.fmg@yandex.com>
2024-06-30 14:39:21 +0200

    chore: vite - use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {svelte} from "@sveltejs/vite-plugin-svelte";
+import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  base: "",
+  plugins: [svelte()]
+});

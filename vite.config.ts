90f1245d5a1e02bebc90e18b76b21740e21aa847
Azgaar <azgaar.fmg@yandex.com>
2024-06-30 18:08:24 +0200

    chore: generate sourcemap on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: "", // relative paths
+  build: {
+    sourcemap: true
+  },
   plugins: [svelte()]
 });
